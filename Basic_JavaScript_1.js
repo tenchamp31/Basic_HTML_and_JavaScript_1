c748fb39ac1227c57086752fc2ac45e96930b345
tenchamp31 <ygandhi191@gmail.com>
2020-10-15 12:45:47 -0400

    Changed function

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,1 +1,10 @@
-alert("Hello, World! I am an alert box!")
+<!DOCTYPE html>
+<html>
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+	
+</html>
